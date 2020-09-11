c65a960402e83baf5eab9b1f1ce6c5bc38e70788
yongsheng1992 <mrzys@outlook.com>
2020-09-11 14:37:42 +0800

    90. 子集 II

--- 90.py
+++ 90.py
@@ -1,7 +1,7 @@
 """
     90. 子集 II
     -----------
-    使用回溯，因为在每一步需要做去重。
+    空间复杂度优化。
 """
 from typing import List
 
@@ -9,15 +9,17 @@ from typing import List
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
+        nums = sorted(nums)
+        n = len(nums)
 
-        def solve(nums, temp):
+        def solve(idx, temp):
             res.append(temp)
 
-            flag = set()
-            for i, num in enumerate(nums):
-                if num not in flag:
-                    flag.add(num)
-                    solve(nums[i+1:], temp + [num])
+            for i in range(idx, n):
+                if i > idx and nums[i] == nums[i-1]:
+                    continue
 
-        solve(sorted(nums), [])
+                solve(i+1, temp + [nums[i]])
+
+        solve(0, [])
         return res
