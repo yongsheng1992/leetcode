73e822743265d420145c7e5d5ca6726dfa1f4754
yongsheng1992 <mrzys@outlook.com>
2020-09-14 11:47:11 +0800

    94. 二叉树的中序遍历

---
+++ 94. 二叉树的中序遍历.py
@@ -0,0 +1,23 @@
+"""
+    94. 二叉树的中序遍历
+    ------------------
+    简单的二叉树的遍历
+
+"""
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
