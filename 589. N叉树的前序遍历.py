a61f7a1c8adc953fb2514c6a0de5f9f9de4842f2
yongsheng1992 <mrzys@outlook.com>
2020-09-16 17:39:50 +0800

    589. N叉树的前序遍历

---
+++ 589. N叉树的前序遍历.py
@@ -0,0 +1,26 @@
+"""
+    589. N叉树的前序遍历
+    ------------------
+"""
+from typing import List
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+
+        res = [root.val]
+
+        if root.children:
+            for child in root.children:
+                res += self.preorder(child)
+
+        return res
