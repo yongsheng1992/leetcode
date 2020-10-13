d076e727ebc324000fe7d0a91214de8ae0b449a0
yongsheng1992 <mrzys@outlook.com>
2020-10-13 19:12:27 +0800

    222. 完全二叉树的节点个数

---
+++ 222. 完全二叉树的节点个数.py
@@ -0,0 +1,19 @@
+"""
+    222. 完全二叉树的节点个数
+    -----------------------
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        return self.countNodes(root.left) + self.countNodes(root.right) + 1
+
