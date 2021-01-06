191f910bd53dcb73542f70953cece8b64fb6f57b
yongsheng1992 <mrzys@outlook.com>
2021-01-06 22:07:19 +0800

    509. 斐波那契数

---
+++ 509. 斐波那契数.py
@@ -0,0 +1,17 @@
+"""
+    509. 斐波那契数
+    --------------
+"""
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+
+        if n == 1:
+            return 1
+
+        a, b = 0, 1
+        for i in range(2, n+1):
+            a, b = b, a + b
+
+        return b
